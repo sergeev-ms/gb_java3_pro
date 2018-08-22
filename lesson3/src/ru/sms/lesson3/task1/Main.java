33e768b45ab68aec60a034fbfb0a96269539be9a
sergeev_ms <sergeevms.33@gmail.com>
2018-08-22 16:15:58 +0300

    Task2.

--- lesson3/src/ru/sms/lesson3/task1/Main.java
+++ lesson3/src/ru/sms/lesson3/task1/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 
 public class Main {
 
-    static final String LESSON3_TASK1_RANDOM_FILE_TXT = "lesson3/randomFile.txt";
+    static final String LESSON3_TASK1_RANDOM_FILE_TXT = "lesson3/Task1_randomFile.txt";
     static final int ARRAY_SIZE = 50;
 
     public static void main(String[] args) {
@@ -31,16 +31,17 @@ public class Main {
         final FileInputStream fileInputStream = new FileInputStream(fileName);
         byte[] bytes = new byte[ARRAY_SIZE];
         fileInputStream.read(bytes);
+        fileInputStream.close();
         return bytes;
     }
 
-    private static void generateFile(byte[] bytes, String fileName) throws IOException {
+    public static void generateFile(byte[] bytes, String fileName) throws IOException {
         final FileOutputStream fileOutputStream = new FileOutputStream(fileName);
         fileOutputStream.write(bytes);
         fileOutputStream.close();
     }
 
-    private static byte[] generateRandomByteArray(int size){
+    public static byte[] generateRandomByteArray(int size){
         Random random = new Random();
         byte[] bytes = new byte[size];
         random.nextBytes(bytes);
