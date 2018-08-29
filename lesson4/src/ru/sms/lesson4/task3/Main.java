3fae25605fd2b48264e78a13dadf36be408d58e4
sergeev_ms <sergeevms.33@gmail.com>
2018-08-29 22:04:35 +0300

    lesson4. task3 fix

--- lesson4/src/ru/sms/lesson4/task3/Main.java
+++ lesson4/src/ru/sms/lesson4/task3/Main.java
@@ -10,22 +10,12 @@ public class Main {
     }
 }
 class MFP {
-    private volatile boolean isPrinting;
-    private volatile boolean isScanning;
     private final Object printerLock = new Object();
     private final Object scannerLock = new Object();
 
     void print(int pages) {
         new Thread(() -> {
             synchronized (printerLock){
-                while (isPrinting) {
-                    try {
-                        printerLock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                isPrinting = true;
                 for (int i = 1; i <= pages; i++) {
                     System.out.println(String.format("Отпечатано %s страницы из %s", i, pages));
                     try {
@@ -34,8 +24,6 @@ class MFP {
                         e.printStackTrace();
                     }
                 }
-                isPrinting = false;
-                printerLock.notifyAll();
             }
         }).start();
     }
@@ -43,14 +31,6 @@ class MFP {
     void scan(int pages){
         new Thread(() -> {
             synchronized (scannerLock){
-                while (isScanning) {
-                    try {
-                        scannerLock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                isScanning = true;
                 for (int i = 1; i <= pages; i++) {
                     System.out.println(String.format("  Отсканировано %s страницы из %s", i, pages));
                     try {
@@ -59,8 +39,6 @@ class MFP {
                         e.printStackTrace();
                     }
                 }
-                isScanning = false;
-                scannerLock.notifyAll();
             }
         }).start();
     }
