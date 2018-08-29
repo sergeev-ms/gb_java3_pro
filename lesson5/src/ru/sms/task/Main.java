2ffcaf40b52dd562227516f8a940e7837b1a1560
sergeev_ms <sergeevms.33@gmail.com>
2018-08-29 22:15:35 +0300

    lesson5. task1 Initial

---
+++ lesson5/src/ru/sms/task/Main.java
@@ -0,0 +1,27 @@
+/*
+Организуем гонки:
+        Все участники должны стартовать одновременно, несмотря на то, что на подготовку у каждого их них уходит разное время.
+        В тоннель не может заехать одновременно больше половины участников (условность).
+        Попробуйте все это синхронизировать.
+        Только после того, как все завершат гонку, нужно выдать объявление об окончании.
+        Можете корректировать классы (в т.ч. конструктор машин) и добавлять объекты классов из пакета util.concurrent.
+*/
+package ru.sms.task;
+
+public class Main {
+    private static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
---
+++ lesson5/src/ru/sms/task/Stage.java
@@ -0,0 +1,10 @@
+package ru.sms.task;
+
+public abstract class Stage {
+    protected int length;
+    protected String description;
+    public String getDescription() {
+        return description;
+    }
+    public abstract void go(Car c);
+}
